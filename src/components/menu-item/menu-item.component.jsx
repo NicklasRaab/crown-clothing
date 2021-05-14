f1cce4ef5023aa06d2b95a6c3efab6db53cf0459
Nicklas Raab <nicklas.raab@gmail.com>
2021-05-14 21:27:08 +0200

    updating our app with a shop page, collection preview component and collection item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,6 +5,7 @@ import { withRouter } from 'react-router-dom';
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
 	return (
 		<div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+			{/* match.url ger adressen där man är just nu. dvs att komponentetn kan användas var som helst och fortsätta på befintlig adress */}
 			<div
 				className='background-image'
 				style={{
